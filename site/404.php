d32f8dfed51604e946f6aca22d42eed2ad834eeb
Hartmann Matthias <matthiworld@gmail.com>
2022-05-22 12:21:15 +0200

    correction faute

--- site/404.php
+++ site/404.php
@@ -7,7 +7,7 @@ include('client/pages/header.html');
             <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
             <lottie-player src="https://assets7.lottiefiles.com/packages/lf20_sdd6siet.json"  background="transparent"  speed="1"  style="width: 75%;"  loop  autoplay></lottie-player>
             <h2>
-                Il semblerait que vous vous êtes perdu.
+                Il semblerait que vous vous soyez perdu.
             </h2>
             <h3>
                 Cliquez sur le bouton pour revenir à la page index.
