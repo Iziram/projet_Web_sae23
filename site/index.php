8c347724336b4c04c14da9b3d0f0a26d27d2e7f8
Hartmann Matthias <matthiworld@gmail.com>
2022-05-25 22:04:15 +0200

    angry boy

--- site/index.php
+++ site/index.php
@@ -23,6 +23,41 @@
  <div id="placer">
 
  </div>
+
+ 
+
+
+ <div class="container">
+ <ul class="collapsible">
+    <li>
+      <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+  </ul>
+
+ </div>
+            
+ <script>
+   document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+ </script>
+
  <?php
 
     include('client/pages/footer.html');
